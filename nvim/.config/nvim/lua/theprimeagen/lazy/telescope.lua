b70047c1a1e411efa82fd7e767992829512c77aa
Svovoniks <svovoniks@gmail.com>
2024-10-27 11:07:35 +0300

    add screenshot

--- nvim/.config/nvim/lua/theprimeagen/lazy/telescope.lua
+++ nvim/.config/nvim/lua/theprimeagen/lazy/telescope.lua
@@ -11,6 +11,9 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>phf', function()
+            builtin.find_files { hidden = true }
+        end)
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
@@ -25,9 +28,8 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>phs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > "), additional_args = { "--hidden" },})
+            builtin.grep_string({ search = vim.fn.input("Grep > "), additional_args = { "--hidden" }, })
         end)
-        vim.keymap.set('n', '<leader>vh' , builtin.help_tags, {})
-   end
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
 }
-
